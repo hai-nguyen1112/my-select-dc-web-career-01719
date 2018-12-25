6a94a4ca34d0f376f4391d6d75891fa2f0285d09
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-25 15:23:21 -0500

    My my select commit

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
- # your code here!
+  if collection == []
+    puts "The block cannot be run"
+  end
+  new_collection = []
+  if block_given?
+    i = 0
+    while i < collection.size
+      a = yield collection[i]
+      i += 1
+      new_collection << a
+    end
+    new_collection
+  else
+    puts "There is no block given"
+  end
+end
+nums = [1, 2, 3, 4, 5]
+my_select(nums) do |i|
+   if i.even?
+     i
+   end
 end
